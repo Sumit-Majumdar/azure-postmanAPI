14bc7ea6a0d119ac03dd9c6aa1567072a62a3dfe
Sumit-Majumdar <136681403+Sumit-Majumdar@users.noreply.github.com>
2025-08-21 18:25:30 +0530

    Add files via upload

---
+++ newman-run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Run collection with environment
+newman run "/mnt/data/Petstore_QA_Automation.postman_collection.json" -e "/mnt/data/Petstore_Local.postman_environment.json" --reporters cli,htmlextra --reporter-htmlextra-export "/mnt/data/newman-report.html"
+
+# Data-driven run (CSV)
+newman run "/mnt/data/Petstore_QA_Automation.postman_collection.json" -e "/mnt/data/Petstore_Local.postman_environment.json" --iteration-data "/mnt/data/pet_data.csv" --reporters cli,htmlextra --reporter-htmlextra-export "/mnt/data/newman-report-data.html"
